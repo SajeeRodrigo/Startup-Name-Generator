3ee750abd5e4b4eabb5477cf2d6020f0ae18fe59
SajeeRodrigo <sajee.thamanga@gmail.com>
2022-02-09 15:46:24 +0530

    second

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,14 +11,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Welcome to flutter',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Mindset Is Everything!'),
         ),
-        body: const Center(
-          child: Text('Hellow world!'),
+       
+        body: Center(
+            child: Text(wordPair.asPascalCase),
+          
         ),
       ),
     );
